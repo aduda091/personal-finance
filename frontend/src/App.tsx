47e03c77519038af77dcf74d9dde39708cd894f5
Alen Duda <alenduda@gmail.com>
2023-05-30 00:08:06 +0200

    header alignment, period placeholder?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Col, Row, Space } from "antd";
 import { useState } from "react";
 
 const { Header, Content, Sider, Footer } = Layout;
@@ -8,9 +8,32 @@ function App() {
     return (
         <>
             <Layout>
-                <Header style={{ display: "flex", alignItems: "center", color: "white" }}>Header</Header>
+                <Header style={{ color: "white" }}>
+                    <Row justify="space-between">
+                        <Col span={18}>
+                            <Space>
+                                <h1>Personal Finance App</h1>
+                            </Space>
+                        </Col>
+                        <Col
+                            span={6}
+                            style={{
+                                textAlign: "right"
+                            }}
+                        >
+                            <Space size="large" align="end">
+                                Period: Date picker goes here? Or in content?
+                            </Space>
+                        </Col>
+                    </Row>
+                </Header>
                 <Layout>
-                    <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)} style={{ color: "white" , height: "100vh"}}>
+                    <Sider
+                        collapsible
+                        collapsed={collapsed}
+                        onCollapse={value => setCollapsed(value)}
+                        style={{ color: "white", height: "100vh" }}
+                    >
                         Sidebar
                     </Sider>
                     <Layout style={{ padding: "0 24px 24px" }}>
@@ -18,7 +41,7 @@ function App() {
                             style={{
                                 padding: 24,
                                 margin: 0,
-                                minHeight: 280,
+                                minHeight: 280
                             }}
                         >
                             Content
