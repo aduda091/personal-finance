d2fdaf57304912c190b5905484ddf3b417202756
Alen Duda <alenduda@gmail.com>
2023-05-29 23:27:35 +0200

    add ant design to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,34 @@
-import './App.css'
+import { Layout } from "antd";
+import { useState } from "react";
+
+const { Header, Content, Sider, Footer } = Layout;
 
 function App() {
-
-  return (
-    <>
-      init
-    </>
-  )
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <>
+            <Layout>
+                <Header style={{ display: "flex", alignItems: "center", color: "white" }}>Header</Header>
+                <Layout>
+                    <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)} style={{ color: "white" , height: "100vh"}}>
+                        Sidebar
+                    </Sider>
+                    <Layout style={{ padding: "0 24px 24px" }}>
+                        <Content
+                            style={{
+                                padding: 24,
+                                margin: 0,
+                                minHeight: 280,
+                            }}
+                        >
+                            Content
+                        </Content>
+                        <Footer style={{ textAlign: "center", color: "white" }}>Footer</Footer>
+                    </Layout>
+                </Layout>
+            </Layout>
+        </>
+    );
 }
 
-export default App
+export default App;
