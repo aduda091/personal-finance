2a94963273f7cbff6c7ebe2dd296c298656cc474
Alen Duda <alenduda@gmail.com>
2023-06-03 21:23:49 +0200

    expense groups crud modal

---
+++ frontend/src/components/ExpenseGroupsModal/ExpenseGroupsModal.tsx
@@ -0,0 +1,92 @@
+import { Modal, ModalFuncProps, Form, Input, Button, notification } from "antd";
+import { FC } from "react";
+import { ExpenseGroup } from "../../gql/graphql";
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_EXPENSE_GROUP_MUTATION = gql`
+    mutation UpdateExpenseGroup($createExpenseGroupInput: CreateExpenseGroupInput) {
+        createExpenseGroup(createExpenseGroupInput: $createExpenseGroupInput) {
+            id
+            name
+        }
+    }
+`;
+
+const UPDATE_EXPENSE_GROUP_MUTATION = gql`
+    mutation UpdateExpenseGroup($updateExpenseGroupInput: UpdateExpenseGroupInput) {
+        updateExpenseGroup(updateExpenseGroupInput: $updateExpenseGroupInput) {
+            id
+            name
+        }
+    }
+`;
+
+export interface ExpenseGroupsModalProps extends ModalFuncProps {
+    editObject?: Omit<ExpenseGroup, "__typename"> | null;
+}
+
+const ExpenseGroupsModal: FC<ExpenseGroupsModalProps> = props => {
+    const { editObject, ...rest } = props;
+
+    const [createExpenseGroup, { loading: createLoading }] = useMutation(CREATE_EXPENSE_GROUP_MUTATION);
+    const [updateExpenseGroup, { loading: updateLoading }] = useMutation(UPDATE_EXPENSE_GROUP_MUTATION);
+
+    const onSubmit = (data: Partial<ExpenseGroup>) => {
+        if (editObject) {
+            updateExpenseGroup({
+                variables: {
+                    updateExpenseGroupInput: {
+                        id: editObject.id,
+                        name: data.name
+                    }
+                }
+            }).then(() => {
+                props.onOk?.();
+                notification.success({ message: "Expense group updated" });
+            });
+        } else {
+            createExpenseGroup({
+                variables: {
+                    createExpenseGroupInput: {
+                        name: data.name
+                    }
+                },
+                refetchQueries: ["ExpenseGroups"]
+            }).then(() => {
+                props.onOk?.();
+                notification.success({ message: "Expense group created" });
+            });
+        }
+    };
+
+    return (
+        <Modal title={`${editObject ? "Edit" : "Add"} Expense group`} {...rest} destroyOnClose footer={null}>
+            <Form
+                initialValues={{
+                    id: editObject?.id ?? "",
+                    name: editObject?.name ?? ""
+                }}
+                onFinish={onSubmit}
+                autoComplete="off"
+                preserve={false}
+            >
+                <Form.Item label="Name" name="name" rules={[{ required: true, message: "Name is required!" }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 20, span: 4 }}>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={createLoading || updateLoading}
+                        disabled={createLoading || updateLoading}
+                    >
+                        {editObject ? "Update" : "Add"}
+                    </Button>
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+};
+
+export default ExpenseGroupsModal;
--- frontend/src/components/ExpenseGroupsTable/ExpenseGroupsTable.tsx
+++ frontend/src/components/ExpenseGroupsTable/ExpenseGroupsTable.tsx
@@ -1,8 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
-import { Button, Space, Table } from "antd";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import { useMemo } from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { Button, Popconfirm, Space, Table, notification } from "antd";
+import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+import { useMemo, useState } from "react";
 import { ExpenseGroup, ExpenseGroupsQuery } from "../../gql/graphql";
+import ExpenseGroupsModal from "../ExpenseGroupsModal/ExpenseGroupsModal";
 
 const EXPENSE_GROUPS_QUERY = gql`
     query ExpenseGroups {
@@ -13,53 +14,96 @@ const EXPENSE_GROUPS_QUERY = gql`
     }
 `;
 
-const columns = [
-    {
-        key: "name",
-        title: "Name",
-        dataIndex: "name"
-    },
-    {
-        key: "actions",
-        title: "Actions",
-        render: (_: unknown, record: ExpenseGroup) => {
-            return (
-                <>
-                    <Space>
-                        <Button
-                            type="primary"
-                            icon={<EditOutlined />}
-                            onClick={() => console.log(`Editing ${record.name}`)}
-                        >
-                            Edit
-                        </Button>{" "}
-                        <Button
-                            type="primary"
-                            danger
-                            icon={<DeleteOutlined />}
-                            onClick={() => console.log(`Deleting ${record.name}`)}
-                        >
-                            Delete
-                        </Button>
-                    </Space>
-                </>
-            );
+const DELETE_EXPENSE_GROUP_MUTATION = gql`
+    mutation DeleteExpenseGroup($deleteExpenseGroupId: Int!) {
+        deleteExpenseGroup(id: $deleteExpenseGroupId) {
+            id
         }
     }
-];
+`;
 
 const ExpenseGroupsTable = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editObject, setEditObject] = useState<ExpenseGroup | null>(null);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+        setEditObject(null);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+        setEditObject(null);
+    };
+
     const { data, loading, error } = useQuery<ExpenseGroupsQuery>(EXPENSE_GROUPS_QUERY);
 
     const dataSource = useMemo(() => data?.expenseGroups?.map(({ id, name }) => ({ key: id, id, name })) ?? [], [data]);
+
+    const [deleteExpenseGroup, { loading: deleteLoading }] = useMutation(DELETE_EXPENSE_GROUP_MUTATION);
+
+    const deleteExpenseGroupHandler = (id: number) => {
+        deleteExpenseGroup({ variables: { deleteExpenseGroupId: id }, refetchQueries: ["ExpenseGroups"] }).then(() => {
+            // todo: notify on success
+            notification.info({ message: "Expense group deleted" });
+        });
+    };
+
+    const columns = [
+        {
+            key: "name",
+            title: "Name",
+            dataIndex: "name"
+        },
+        {
+            key: "actions",
+            title: "Actions",
+            render: (_: unknown, record: ExpenseGroup) => {
+                return (
+                    <>
+                        <Space>
+                            <Button
+                                type="primary"
+                                icon={<EditOutlined />}
+                                onClick={() => {
+                                    setEditObject(record);
+                                    showModal();
+                                }}
+                            >
+                                Edit
+                            </Button>{" "}
+                            <Popconfirm
+                                title="Delete the expense group"
+                                description="Are you sure to delete this expense group?"
+                                onConfirm={() => deleteExpenseGroupHandler(record.id)}
+                                okText="Yes"
+                                cancelText="Cancel"
+                            >
+                                <Button type="primary" danger icon={<DeleteOutlined />}>
+                                    Delete
+                                </Button>
+                            </Popconfirm>
+                        </Space>
+                    </>
+                );
+            }
+        }
+    ];
 
     if (error) {
         return <h3>Error fetching expense groups</h3>;
     }
     return (
         <div>
-            <h3>Expense groups</h3>
-            <Table columns={columns} dataSource={dataSource} loading={loading} pagination={false} />
+            <h3>
+                Expense groups <Button type="primary" icon={<PlusOutlined />} onClick={() => showModal()} />
+            </h3>
+            <Table columns={columns} dataSource={dataSource} loading={loading || deleteLoading} pagination={false} />
+            <ExpenseGroupsModal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} editObject={editObject} />
         </div>
     );
 };
